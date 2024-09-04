501515f23a22b035311f12af015114d08eb0f9fb
lhccong <liuhuaicong@zhut.cn>
2024-09-04 11:39:52 +0800

    feat：新增评审日志写入仓库

--- code-lens-sdk/src/main/java/com/cong/middleware/sdk/OpenAiCodeReview.java
+++ code-lens-sdk/src/main/java/com/cong/middleware/sdk/OpenAiCodeReview.java
@@ -5,17 +5,28 @@ import com.cong.middleware.sdk.domain.model.ChatCompletionRequest;
 import com.cong.middleware.sdk.domain.model.ChatCompletionSyncResponse;
 import com.cong.middleware.sdk.domain.model.Model;
 import com.cong.middleware.sdk.types.utils.BearerTokenUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
 public class OpenAiCodeReview {
 
     public static void main(String[] args) throws Exception {
-        String apiKey = "5ec1978b2e4dabc5f2e719b509275202.DKpwZDd3tGImaxTH";
+
+        String apiKey = System.getenv("GLM_API_KEY");
+
+        String token = System.getenv("GITHUB_TOKEN");
+        if (null == token || token.isEmpty()) {
+            throw new RuntimeException("token is null");
+        }
 
         System.out.println("测试执行");
         //1、代码检出
@@ -39,6 +50,10 @@ public class OpenAiCodeReview {
         // 2. chatglm 代码评审
         String log = codeReview(apiKey, String.valueOf(diffCode));
         System.out.println("code review：" + log);
+
+        // 3. 写入评审日志
+        String logUrl = writeLog(token, log);
+        System.out.println("writeLog：" + logUrl);
     }
 
     private static String codeReview(String apiKeySecret, String diffCode) throws Exception {
@@ -76,6 +91,43 @@ public class OpenAiCodeReview {
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
+    }
+
+    private static String writeLog(String token, String log) throws Exception {
+        Git git = Git.cloneRepository()
+                .setURI("https://github.com/fuzhengwei/openai-code-review-log.git")
+                .setDirectory(new File("repo"))
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(token, ""))
+                .call();
+
+        String dateFolderName = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        File dateFolder = new File("repo/" + dateFolderName);
+        if (!dateFolder.exists()) {
+            dateFolder.mkdirs();
+        }
+
+        String fileName = generateRandomString() + ".md";
+        File newFile = new File(dateFolder, fileName);
+        try (FileWriter writer = new FileWriter(newFile)) {
+            writer.write(log);
+        }
+
+        git.add().addFilepattern(dateFolderName + "/" + fileName).call();
+        git.commit().setMessage("Add new file via GitHub Actions").call();
+        git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(token, "")).call();
+
+        System.out.println("Changes have been pushed to the repository.");
+
+        return "https://github.com/lhccong/code-lens-logs/blob/master/" + dateFolderName + "/" + fileName;
+    }
+    private static String generateRandomString() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(12);
+        for (int i = 0; i < 12; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return sb.toString();
     }
 
     private static HttpURLConnection getHttpURLConnection(String token) throws IOException {
