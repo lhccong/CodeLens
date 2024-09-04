01a6797890e772f5c8a9168cb740cbbef5c3798b
lhccong <liuhuaicong@zhut.cn>
2024-09-04 09:46:05 +0800

    feat：检出代码

--- code-lens-sdk/src/main/java/com/cong/middleware/sdk/OpenAiCodeReview.java
+++ code-lens-sdk/src/main/java/com/cong/middleware/sdk/OpenAiCodeReview.java
@@ -1,9 +1,32 @@
 package com.cong.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("测试执行");
+        //1、代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine())!= null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code:" + exitCode);
+
+        System.out.println("评审代码：" + diffCode);
     }
 
 }
