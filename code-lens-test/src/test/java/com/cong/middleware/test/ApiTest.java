a9b72b0150c7b9668fb37d76583a388fe89a73ba
lhccong <liuhuaicong@zhut.cn>
2024-09-04 10:03:42 +0800

    feat：检出代码v2.0

--- code-lens-test/src/test/java/com/cong/middleware/test/ApiTest.java
+++ code-lens-test/src/test/java/com/cong/middleware/test/ApiTest.java
@@ -2,18 +2,16 @@ package com.cong.middleware.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 @Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("aaaa"));
+        System.out.println("aaaa");
+        System.out.println("aaaa1");
+        System.out.println("aaaa2");
     }
 
 }
